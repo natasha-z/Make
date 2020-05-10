57ffb2adfa5fdbc899815e3f5dc12295e598844c
nAtaman <ataman@macpaw.com>
2020-05-10 23:40:57 +0300

    initial commit, image picker

--- Make/ViewController.swift
+++ Make/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .lightText
+        
     }
-
-
+    
+    @IBAction func imageTap(_ sender: UITapGestureRecognizer) {
+        selectImage()
+    }
+    
+    func selectImage() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        
+        present(picker, animated: true, completion: nil)
+    }
+}
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        imageView.image = info[.originalImage] as? UIImage
+    }
 }
 
