4add62a1d8b1bea0c8a7f2a5cf9bbab1d62c0a4a
nAtaman <ataman@macpaw.com>
2020-05-10 22:22:24 +0300

    Initial Commit

---
+++ Make/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Make
+//
+//  Created by Ataman on 10.05.2020.
+//  Copyright © 2020 Ataman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
